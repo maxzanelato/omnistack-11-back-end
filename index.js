60baebb9a43ffe6fca2f20a790d433d0a917d256
Max Zanelato <maxzanelato@gmail.com>
2020-03-23 13:21:18 -0300

    Comentários

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// Fachada HTTP Rest Controller
 const express = require("express");
 
+// Instancia o express
 const app = express();
 
+// Adiciona uma rota get
 app.get("/", (request, response) => {
   return response.json({
     evento: "Semana Omnistack 11.0",
@@ -9,4 +12,5 @@ app.get("/", (request, response) => {
   });
 });
 
+// Ouvir na porta 3333
 app.listen(3333);
