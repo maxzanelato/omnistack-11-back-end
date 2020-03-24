9d1c7137aed5e565fde0a2aa49660e4981b77972
Max Zanelato <maxzanelato@gmail.com>
2020-03-24 18:40:36 -0300

    Adição de cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 // Fachada HTTP Rest Controller
 const express = require("express");
+// Importação do cors
+const cors = require('cors');
 // Importa o arquivo de rotas HTTP
 const routes = require("./routes");
 
 // Instancia o express
 const app = express();
+
 // Diz ao express que o body será em formato json
 app.use(express.json());
+// Diz que a aplicação tem que utilizar cors
+app.use(cors());
 // Diz para usar aquele arquivo de rotas
 app.use(routes);
 
