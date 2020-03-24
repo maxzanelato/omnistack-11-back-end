019eec922f353547ac542a787a0a204507e8cce0
Max Zanelato <maxzanelato@gmail.com>
2020-03-24 16:15:02 -0300

    Instalação do Nodemon para hot deploy

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require("express");
 
 // Instancia o express
 const app = express();
+// Diz ao express que o body será em formato json
+app.use(express.json());
 
 // Adiciona uma rota get
-app.get("/", (request, response) => {
+app.get("/users", (request, response) => {
   return response.json({
     evento: "Semana Omnistack 11.0",
     aluno: "Max Zanelato"
