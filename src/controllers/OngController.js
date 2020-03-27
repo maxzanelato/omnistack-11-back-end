97a46b1e5e122a675270649f93f9f0b44834f6f7
Max Zanelato <maxzanelato@gmail.com>
2020-03-27 14:45:58 -0300

    Teste unitário com JEST.

--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -1,25 +1,19 @@
-const connection = require('../database/connection');
-const crypto = require('crypto');
+const connection = require("../database/connection");
+const generateUniqueId = require("../utils/generateUniqueId");
 
 module.exports = {
   async listAll(request, response) {
-    const ongs = await connection('ongs').select('*');
+    const ongs = await connection("ongs").select("*");
 
     return response.json(ongs);
   },
 
   async create(request, response) {
-    const {
-      name,
-      email,
-      whatsapp,
-      city,
-      uf
-    } = request.body;
+    const { name, email, whatsapp, city, uf } = request.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
-    await connection('ongs').insert({
+    await connection("ongs").insert({
       id,
       name,
       email,
@@ -32,4 +26,4 @@ module.exports = {
       id
     });
   }
-}
+};
