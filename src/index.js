320f017f17f3e4d6d81f362af328ad0fe425d4d7
Max Zanelato <maxzanelato@gmail.com>
2020-03-27 14:13:53 -0300

    Adição de validação.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Fachada HTTP Rest Controller
 const express = require("express");
 // Importação do cors
-const cors = require('cors');
+const cors = require("cors");
+// Importação do celebrate para tratar erros de forma correta e evitar HTTP 500 na validação.
+const { errors } = require("celebrate");
 // Importa o arquivo de rotas HTTP
 const routes = require("./routes");
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 // Diz para usar aquele arquivo de rotas
 app.use(routes);
+// Diz para a aplicação utilizar o tratador de erros do celebrate
+app.use(errors());
 
 // Ouvir na porta 3333
 app.listen(3333);
--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,72 @@
-const express = require('express');
+const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 
-const OngController = require('./controllers/OngController');
-const IncidentController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController');
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
 // Adiciona uma rotas de escuta
 
 // Rota de login
-routes.post('/sessions', SessionController.create);
+routes.post("/sessions", SessionController.create);
 
 // Rota para listar todas as ongs
-routes.get('/ongs', OngController.listAll);
+routes.get("/ongs", OngController.listAll);
 // Rota para criar uma ong
-routes.post('/ongs', OngController.create);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
 
 // Rota para listar todas as incidências
-routes.get('/incidents', IncidentController.listAll)
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.listAll
+);
 // Rota para criar um incident
-routes.post('/incidents', IncidentController.create);
+routes.post("/incidents", IncidentController.create);
 // Rota para deletar
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentController.delete
+);
 
 // Rota para lista um profile específico
-routes.get('/profiles', ProfileController.find);
+routes.get(
+  "/profiles",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.find
+);
 
 module.exports = routes;
