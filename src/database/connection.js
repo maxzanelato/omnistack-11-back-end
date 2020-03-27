4a931ed5bc79029f4bc5a5cb14eee3a576a08519
Max Zanelato <maxzanelato@gmail.com>
2020-03-27 14:53:08 -0300

    Configuração de environment.

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,12 @@
-const knex = require('knex');
-const configuration = require('../../knexfile');
+const knex = require("knex");
+const configuration = require("../../knexfile");
+
+const config =
+  process.env.NODE_ENV === "test"
+    ? configuration.test
+    : configuration.development;
 
 // Seleciona a conexão de environment de desenvolvimento em vez de PROD ou HOM
-const connection = knex(configuration.development);
+const connection = knex(config);
 
 module.exports = connection;
